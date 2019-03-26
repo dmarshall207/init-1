052b12bdd9b2cd1ec204f645065c788ec5bcb848
dlmarshall207 <dlmarshall207@gmail.com>
2019-03-26 12:02:16 -0500

    3/26

---
+++ b1.ps1
@@ -0,0 +1,5 @@
+git config --global user.name "dlmarshall207"
+git config --global user.email "dlmarshall207@gmail.com"
+git config --global user.name
+git config --global user.email
+git config --global --list
