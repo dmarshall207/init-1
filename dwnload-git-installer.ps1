c67777795d9695a2cb97ab72d0c1db7ba1586363
dmarshall207 <47430930+dmarshall207@users.noreply.github.com>
2019-03-26 11:15:11 -0500

    Create dwnload-git-installer.ps1

---
+++ dwnload-git-installer.ps1
@@ -0,0 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+wget -o Git-installer-64-bit.exe https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/Git-2.21.0-64-bit.exe
