5687840624f83e1bae48c8a9689d4965e9ce1d9c
dlmarshall207 <dlmarshall207@gmail.com>
2019-03-26 12:11:01 -0500

    3/26

---
+++ c1.ps1
@@ -0,0 +1,12 @@
+cd c:\sto\nodejs
+mkdir my
+
+cd $StoRoot
+git clone https://github.com/dmarshall207/bin.git
+git clone https://github.com/dmarshall207/my-doc.git
+
+cd $StoMy
+git clone https://github.com/dmarshall207/my-dev-support.git
+git clone https://github.com/dmarshall207/node_modules.git
+
+
