5b01f4dd4a1e0f2144de9656436442839b056a72
dlmarshall207 <dlmarshall207@gmail.com>
2019-03-27 12:34:29 -0500

    3/27  added dwnload for mongodb

---
+++ dwnload-mongodb-binary-x64.ps1
@@ -0,0 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+wget -o  mongodb-win32-x86_64-2008plus-ssl-4.0.7.zip  https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-4.0.7.zip 
